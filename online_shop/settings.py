4650865493ff274f7dda1a903ffd91ea37e2ef9d
danyshman <danyshman.azamatov@gmail.com>
2020-01-27 20:20:02 +0300

    config static files

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -136,7 +136,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # Static
-STATIC_ROOT = os.path.join(BASE_DIR, 'online_shop/static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static_files')
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_files')
