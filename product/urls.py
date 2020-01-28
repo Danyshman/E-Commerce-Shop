484f5a9e4150a68449138c0f1262c0fdf4e30e78
danyshman <danyshman.azamatov@gmail.com>
2020-01-28 20:29:01 +0300

    add shop-grid.html and related images to that page

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
 
+app_name = 'product'
+
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('shop/', shop_page, name='shop_page')
 ]
--- product/views.py
+++ product/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def shop_page(request):
+    print('in view')
+    return render(request, 'shop-grid.html')
