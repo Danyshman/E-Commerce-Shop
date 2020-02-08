9df909cc218fb6f0051949993651c3e3d1f6c737
danyshman <danyshman.azamatov@gmail.com>
2020-02-08 15:18:21 +0300

    implement support_ticket model

--- support_ticket/models.py
+++ support_ticket/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from constants import *
 
-# Create your models here.
+
+class SupportTicket(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, null=True, related_name='support_tickets')
+    subject = models.CharField(max_length=255, null=True, blank=True)
+    status = models.CharField(max_length=255, choices=SUPPORT_TICKET_STATUS, null=True, blank=True)
+    type = models.CharField(max_length=255, choices=SUPPORT_TICKET_TYPE, null=True, blank=True)
+    priority = models.CharField(max_length=255, choices=SUPPORT_TICKET_PRIORITY, null=True, blank=True)
+    date_submitted = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+
