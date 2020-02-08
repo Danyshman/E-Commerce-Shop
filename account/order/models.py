455cd3605984c248dce124f5ef766368797814fa
danyshman <danyshman.azamatov@gmail.com>
2020-02-08 14:47:19 +0300

    add manytomany products field

--- account/order/models.py
+++ account/order/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 import uuid
 from constants import ORDER_STATUS
-from account.models import User
+from django.conf import settings
 
 
 class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    date_purchased = models.DateField(auto_now_add=True)
     status = models.CharField(max_length=255, choices=ORDER_STATUS, null=False, default='In Progress')
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='orders', null=True)
     total_price = models.PositiveIntegerField(blank=True, null=True)
+    date_purchased = models.DateField(auto_now_add=True)
 
